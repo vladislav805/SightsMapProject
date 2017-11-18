5889614ca7e33e6310d06961180949b3479766b0
Vladislav Veluga <vladislav805@yandex.com>
2017-11-18 15:26:29 +0300

    v0.9.8.5: added query for remove events when point remove

--- modules/Method/Point/Remove.php
+++ modules/Method/Point/Remove.php
@@ -32,11 +32,12 @@
 // TODO: remove photos by split method and remove files
 			$sql = [
 				sprintf("DELETE FROM `point` WHERE `ownerId` = '%d' AND `pointId` = '%d' LIMIT 1", $ownerId, $this->pointId),
-				sprintf("DELETE FROM `photo` WHERE `photoId` IN (SELECT `photoId` FROM  `pointPhoto` WHERE `pointId` = '%d')", $this->pointId),
+				sprintf("DELETE FROM `photo` WHERE `photoId` IN (SELECT `photoId` FROM `pointPhoto` WHERE `pointId` = '%d')", $this->pointId),
 				sprintf("DELETE FROM `pointPhoto` WHERE `pointId` = '%d' LIMIT 1", $this->pointId),
 				sprintf("DELETE FROM `pointMark` WHERE `pointId` = '%d' LIMIT 1", $this->pointId),
 				sprintf("DELETE FROM `pointVisit` WHERE `pointId` = '%d' LIMIT 1", $this->pointId),
-				sprintf("DELETE FROM `comment` WHERE `pointId` = '%d' LIMIT 1", $this->pointId)
+				sprintf("DELETE FROM `comment` WHERE `pointId` = '%d' LIMIT 1", $this->pointId),
+				sprintf("DELETE FROM `event` WHERE `type` IN (1, 7, 8, 9, 11) AND `subjectId` = '%d'", $this->pointId)
 			];
 
 			$res = 0;
