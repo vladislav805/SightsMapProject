e61caa6c6dc0116719366bb5f7ba8ef4a23ddef3
Vladislav Veluga <vladislav805@yandex.com>
2018-03-24 00:12:53 +0300

    Edit footer

--- pager/__footer.php
+++ pager/__footer.php
@@ -5,6 +5,8 @@
 					<div class="footer-logo">Sights map</div>
 					<ul>
 						<li><a href="/" target="_blank">Главная</a></li>
+						<li><a href="/map" target="_blank">Карта</a></li>
+						<li><a href="https://docs.google.com/document/d/18sEUblZnA51Ni_6wAhrqTqCr6if3mkETaEyasnCH1rM/edit?usp=sharing" target="_blank">API</a></li>
 					</ul>
 				</div>
 				<div class="footer-right">
