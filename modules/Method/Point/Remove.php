c62213a12940b70031cb09a74fcbce6d578e5b72
Vladislav Veluga <vladislav805@yandex.com>
2018-04-07 17:10:41 +0300

    Remove shit code. Added constraints to db.

--- modules/Method/Point/Remove.php
+++ modules/Method/Point/Remove.php
@@ -36,22 +36,8 @@
 			assertOwner($main, $point->getOwnerId(), ERROR_ACCESS_DENIED);
 
 			$ownerId = $point->getOwnerId();
-// TODO: remove photos by split method and remove files
-			$sql = [
-				sprintf("DELETE FROM `point` WHERE `ownerId` = '%d' AND `pointId` = '%d' LIMIT 1", $ownerId, $this->pointId),
-				sprintf("DELETE FROM `photo` WHERE `photoId` IN (SELECT `photoId` FROM `pointPhoto` WHERE `pointId` = '%d')", $this->pointId),
-				sprintf("DELETE FROM `pointPhoto` WHERE `pointId` = '%d' LIMIT 1", $this->pointId),
-				sprintf("DELETE FROM `pointMark` WHERE `pointId` = '%d' LIMIT 1", $this->pointId),
-				sprintf("DELETE FROM `pointVisit` WHERE `pointId` = '%d' LIMIT 1", $this->pointId),
-				sprintf("DELETE FROM `comment` WHERE `pointId` = '%d' LIMIT 1", $this->pointId),
-				sprintf("DELETE FROM `event` WHERE `type` IN (1, 7, 8, 9, 11) AND `subjectId` = '%d'", $this->pointId)
-			];
 
-			$res = 0;
-
-			foreach ($sql as $sqlItem) {
-				$res += $db->query($sqlItem, DatabaseResultType::AFFECTED_ROWS);
-			}
+			$res = $db->query(sprintf("DELETE FROM `point` WHERE `ownerId` = '%d' AND `pointId` = '%d' LIMIT 1", $ownerId, $this->pointId), DatabaseResultType::AFFECTED_ROWS);
 
 			return (boolean) $res;
 		}
