eb582afbebf7ce5caee98cbda4ac3e6fd21a0670
Vladislav Veluga <vladislav805@yandex.com>
2018-04-15 22:13:12 +0300

    photos.get with `pointId` now query is smart (IN (...) replaced by `t1` = `t2` ...)

--- modules/Method/Photo/Get.php
+++ modules/Method/Photo/Get.php
@@ -44,11 +44,25 @@
 				throw new APIException(ERROR_NO_PARAM);
 			}
 
+			// SELECT * FROM `photo` lEFT JOIN `user` ON `user`.`userId` = `photo`.`photoId` WHERE `user`.`userId` = '1' ORDER BY `photo`.`photoId` DESC
 			if ($this->ownerId) {
 				$sql = sprintf("SELECT * FROM `photo` WHERE `ownerId` = '%d' AND `type` = '%d' ORDER BY `photoId` DESC LIMIT " . ((int) $this->offset) . "," . ((int) $this->count), $this->ownerId, Photo::TYPE_PROFILE);
 
 			} else {
-				$sql = sprintf("SELECT * FROM `photo` WHERE `photoId` IN (SELECT `photoId` FROM `pointPhoto` WHERE `pointId` = '%d') AND `type` = '%d' ORDER BY `photoId` DESC LIMIT " . ((int) $this->offset) . "," . ((int) $this->count), $this->pointId, Photo::TYPE_POINT);
+				$sql = sprintf("
+SELECT
+	*
+FROM
+	`photo`,
+	`pointPhoto`
+WHERE
+	`photo`.`type` = '%d' AND
+	`pointPhoto`.`pointId` = '%d' AND
+	`photo`.`photoId` = `pointPhoto`.`photoId`
+ORDER BY
+`photo`.`photoId`
+ASC LIMIT " . ((int) $this->offset) . "," . ((int) $this->count),
+					Photo::TYPE_POINT, $this->pointId);
 			}
 
 			$items = $db->query($sql, DatabaseResultType::ITEMS);
