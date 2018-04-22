bdca5e7fb99471fdf7f5df963f9cbd01b568eaf1
Vladislav Veluga <vladislav805@yandex.com>
2018-04-22 09:33:37 +0300

    Update footer

--- pager/__footer.php
+++ pager/__footer.php
@@ -11,7 +11,7 @@
 				</div>
 				<div class="footer-right">
 					<ul>
-						<li><a href="//velu.ga/" target="_blank">velu.ga</a></li>
+						<li><a href="//velu.ga/" target="_blank">velu.ga</a> &copy; 2015&ndash;2018</li>
 					</ul>
 				</div>
 			</footer>
