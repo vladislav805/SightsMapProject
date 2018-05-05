0a8ba55cf025db7c50a7ae6745c4802b84b617d6
Vladislav Veluga <vladislav805@yandex.com>
2018-05-05 19:15:58 +0300

    Add link for search in footer

--- pager/__footer.php
+++ pager/__footer.php
@@ -4,8 +4,9 @@
 				<div class="footer-left">
 					<div class="footer-logo">Sights map</div>
 					<ul>
-						<li><a href="/" target="_blank">Главная</a></li>
-						<li><a href="/map" target="_blank">Карта</a></li>
+						<li><a href="/">Главная</a></li>
+						<li><a href="/map">Карта</a></li>
+						<li><a href="/place/search">Поиск</a></li>
 						<li><a href="https://docs.google.com/document/d/18sEUblZnA51Ni_6wAhrqTqCr6if3mkETaEyasnCH1rM/edit?usp=sharing" target="_blank">API</a></li>
 					</ul>
 				</div>
