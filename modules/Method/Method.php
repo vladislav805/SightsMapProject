82ef9ad4ba628dbe31d055c99c05544b7a7e5b2d
Vladislav Veluga <vladislav805@yandex.com>
2018-05-09 19:06:24 +0300

    Refactor and add 'trim'-handler for string values for Method parser

--- modules/Method/Method.php
+++ modules/Method/Method.php
@@ -9,18 +9,20 @@
 		public function __construct($request) {
 			if (is_array($request)) {
 				foreach ($request as $key => $value) {
-					if (property_exists($this, $key)) {
-						$this->{$key} = $value;
-					}
+					$this->putParam($key, $value);
 				}
 			} elseif (is_object($request) && get_class($request) === "Model\\Params") {
 				/** @var Params $request */
 				$params = $request->getAll();
 				foreach ($params as $key => $value) {
-					if (property_exists($this, $key)) {
-						$this->{$key} = $value;
-					}
+					$this->putParam($key, $value);
 				}
+			}
+		}
+
+		private function putParam($key, $value) {
+			if (property_exists($this, $key)) {
+				$this->{$key} = is_string($value) ? trim($value) : $value;
 			}
 		}
 
