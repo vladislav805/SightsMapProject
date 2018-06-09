89e122cb7e217787e1f8e223e482cecbe0c0f8d8
Vladislav Veluga <vladislav805@yandex.com>
2018-06-09 17:43:47 +0300

    API: Add left join for photo into points.getById

--- modules/Method/Point/GetById.php
+++ modules/Method/Point/GetById.php
@@ -27,8 +27,29 @@
 		 * @throws APIException
 		 */
 		public function resolve(IController $main) {
-			$stmt = $main->makeRequest("SELECT * FROM `point` LEFT JOIN `city` ON `city`.`cityId` = `point`.`cityId` WHERE `pointId` = ? LIMIT 1");
-			$stmt->execute([$this->pointId]);
+			$sql = <<<SQL
+SELECT
+	`point`.*,
+	`city`.`name`,
+	`photo`.`ownerId` AS `photoOwnerId`,
+	`photo`.`photoId`,
+	`photo`.`date` AS `photoDate`,
+	`photo`.`path`,
+	`photo`.`photo200`,
+	`photo`.`photoMax`
+FROM
+	`point`
+		LEFT JOIN `city` ON `city`.`cityId` = `point`.`cityId`
+		LEFT JOIN `pointPhoto` ON `pointPhoto`.`pointId` = `point`.`pointId`
+		LEFT JOIN `photo` ON `pointPhoto`.`photoId` = `photo`.`photoId`
+WHERE
+	`point`.`pointId` = :pointId 
+GROUP BY `point`.`pointId`
+LIMIT 1
+SQL;
+
+			$stmt = $main->makeRequest($sql);
+			$stmt->execute([":pointId" => $this->pointId]);
 			$item = $stmt->fetch(PDO::FETCH_ASSOC);
 
 			if (!$item) {
