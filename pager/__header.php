f611ac4ff7c6a2e7868d566cb0b96370ea921aef
Vladislav Veluga <vladislav805@yandex.com>
2018-06-09 19:57:42 +0300

    Make userlink with hover-effect

--- pager/__header.php
+++ pager/__header.php
@@ -18,25 +18,21 @@
 				<? if ($mainController->getSession()) { ?>
 				<div id="head-events" data-count="0" class="head-events material-icons head-element">&#xe7f4;</div>
 				<? } ?>
-				<div class="head-user">
+				<div class="head-user head-element">
 					<?
 						if ($mainController->getSession()) {
 							$u = $mainController->getUser();
 					?>
-					<div class="head-user-info head-element">
-						<h3 id="hatName"><?=htmlspecialchars($u->getFirstName());?></h3>
-						<h4 id="hatLogin">@<?=htmlspecialchars($u->getLogin());?></h4>
-					</div>
 					<img class="head-user-photo" id="hatPhoto" src="<?=htmlspecialchars($u->getPhoto()->getUrlThumbnail());?>" alt="" />
 					<div class="head-dd-menu">
-						<div class="head-dd-item" onclick="Profile.requestUserInfo(0);">Профиль</div>
-						<div class="head-dd-item" onclick="Profile.showSettings();">Настройки профиля</div>
-						<div class="head-dd-item" onclick="Main.closeSession();">Выход</div>
+						<a class="head-dd-item" href="/user/<?=$u->getLogin();?>">Профиль</a>
+						<a class="head-dd-item" href="/places/my">Места</a>
+						<a class="head-dd-item" href="/login?act=logout">Выход</a>
 					</div>
 					<?
 						} else {
 					?>
-					<a href="/login" class="head-user-auth head-element" onclick="Profile.showLogin();">Авторизация &raquo;</a>
+					<a href="/login" class="head-user-auth" onclick="Profile.showLogin();">Авторизация &raquo;</a>
 					<?
 						}
 					?>
