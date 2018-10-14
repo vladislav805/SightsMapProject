ee6dc9d27129aeffa6af6d764a11d1f52ad1e84a
Vladislav Veluga <vladislav805@yandex.com>
2018-10-15 00:59:27 +0300

    Add points.getRandom

---
+++ modules/Method/Point/GetRandom.php
@@ -0,0 +1,32 @@
+<?
+
+	namespace Method\Point;
+
+	use Method\APIPublicMethod;
+	use Model\IController;
+	use Model\Point;
+	use PDO;
+
+	/**
+	 * Возвращает рандомное/случайное место
+	 * @package Method\Point
+	 */
+	class GetRandom extends APIPublicMethod {
+
+		/**
+		 * @param IController $main
+		 * @return Point
+		 */
+		public function resolve(IController $main) {
+			$res = $main->getDatabaseProvider()->query("SELECT COUNT(*) FROM `point`");
+			$res->execute();
+			list($count) = $res->fetch(PDO::FETCH_NUM);
+
+			$stmt = $main->getDatabaseProvider()->query(sprintf("SELECT `pointId` FROM `point` LIMIT %d, 1", rand(0, $count - 1)));
+			$stmt->execute();
+
+			list($pointId) = $stmt->fetch(PDO::FETCH_NUM);
+
+			return $main->perform(new GetById(["pointId" => (int) $pointId]));
+		}
+	}
