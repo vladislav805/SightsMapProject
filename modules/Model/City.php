52e441af86ce7ddfff20b5f8d13d3f2ccb4e231b
Vladislav Veluga <vladislav805@yandex.com>
2018-10-21 23:57:22 +0300

    Add for city field `parentId`

--- modules/Model/City.php
+++ modules/Model/City.php
@@ -10,10 +10,14 @@
 		/** @var string */
 		private $name;
 
+		/** @var int|null */
+		private $parentId;
+
 
 		public function __construct($d) {
 			$this->cityId = (int) $d["cityId"];
 			$this->name = $d["name"];
+			$this->parentId = (int) $d["parentId"];
 		}
 
 		/**
@@ -32,12 +36,20 @@
 		}
 
 		/**
+		 * @return int|null
+		 */
+		public function getParentId() {
+			return $this->parentId;
+		}
+
+		/**
 		 * @return array
 		 */
 		public function jsonSerialize() {
 			return [
 				"cityId" => $this->cityId,
-				"name" => $this->name
+				"name" => $this->name,
+				"parentId" => $this->parentId > 0 ? $this->parentId : null
 			];
 		}
 	}
