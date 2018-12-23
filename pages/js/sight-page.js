f5ca73393ee165491291e44ca7e13402347cab20
Vladislav Veluga <vladislav805@yandex.com>
2018-12-23 23:45:49 +0300

    ECMAScript sight-page.js

--- pages/js/sight-page.js
+++ pages/js/sight-page.js
@@ -1,12 +1,14 @@
-var Sight = {
+"use strict";
+
+const Sight = {
 
 	setVisitState: function(node) {
 		/** @var {{change: boolean, state: {visited: int, desired: int}}} result */
-		API.points.setVisitState(+node.dataset.pid, +node.dataset.visitState).then(function(result) {
-			var wrap = node.parentNode;
+		API.points.setVisitState(+node.dataset.pid, +node.dataset.visitState).then(result => {
+			const wrap = node.parentNode;
 			wrap.dataset.visitState = node.dataset.visitState;
 
-			var counts = wrap.querySelectorAll("var");
+			const counts = wrap.querySelectorAll("var");
 			counts[1].textContent = String(result.state.visited);
 			counts[2].textContent = String(result.state.desired);
 		}).catch(function(error) {
@@ -16,9 +18,8 @@ var Sight = {
 
 	setRating: function(node, rating) {
 		/** @var {{change: boolean, rating: int}} result */
-		API.rating.set(+node.dataset.pid, rating).then(function(result) {
-			var wrap = node.parentNode;
-			wrap.querySelector("strong").textContent = String(result.rating);
+		API.rating.set(+node.dataset.pid, rating).then(result => {
+			node.parentNode.querySelector("strong").textContent = String(result.rating);
 		}).catch(function(error) {
 			console.error(error);
 		});
@@ -28,7 +29,7 @@ var Sight = {
 		node.disabled = true;
 		node.innerHTML = "Подтверждение ... ";
 		var newState = !+node.dataset.nowState;
-		API.points.setVerify(+node.dataset.pid, newState).then(function(result) {
+		API.points.setVerify(+node.dataset.pid, newState).then(result => {
 			node.innerHTML = "Подтверждение = ";
 			node.dataset.nowState = String(+newState);
 			node.disabled = false;
@@ -39,11 +40,10 @@ var Sight = {
 		node.disabled = true;
 		node.innerHTML = "Архивирование ... ";
 		var newState = !+node.dataset.nowState;
-		API.points.setArchived(+node.dataset.pid, newState).then(function(result) {
+		API.points.setArchived(+node.dataset.pid, newState).then(result => {
 			node.innerHTML = "Архивирование = ";
 			node.dataset.nowState = String(+newState);
 			node.disabled = false;
 		});
 	}
-
 };
