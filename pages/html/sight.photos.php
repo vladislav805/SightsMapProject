b62532cd6373306f3d6c61e4868bb4f1c9559860
Vladislav Veluga <vladislav805@yandex.com>
2018-12-25 23:44:05 +0300

    Add data-noAjax for photos in sight page

--- pages/html/sight.photos.php
+++ pages/html/sight.photos.php
@@ -17,7 +17,7 @@
 			/** @var \Model\User $author */
 			$author = $_users[$photo->getOwnerId()];
 ?>
-<a href="<?=$photo->getUrlOriginal();?>" data-caption="<?=htmlSpecialChars(sprintf('Загружено %s пользователем <a href="/user/%s" target="_blank">@%s</a>', getRelativeDate($photo->getDate()), $author->getLogin(), $author->getLogin()));?>">
+<a href="<?=$photo->getUrlOriginal();?>" data-caption="<?=htmlSpecialChars(sprintf('Загружено %s пользователем <a href="/user/%s" target="_blank">@%s</a>', getRelativeDate($photo->getDate()), $author->getLogin(), $author->getLogin()));?>" data-noAjax>
 	<img src="<?=$photo->getUrlThumbnail();?>" alt="" data-src-big="<?=$photo->getUrlOriginal();?>" />
 </a>
 <?
