f4e1089bd493938bb8ed1cb4edf3bba7597b5e1c
Vladislav Veluga <vladislav805@yandex.com>
2019-01-09 23:41:49 +0300

    Add markIds for \Method\Mark\GetById

--- modules/Method/Mark/GetById.php
+++ modules/Method/Mark/GetById.php
@@ -15,17 +15,37 @@
 	 */
 	class GetById extends APIPublicMethod {
 
-		/** @var int */
+		/** @var int|null */
 		protected $markId;
+
+		/** @var int[]|null */
+		protected $markIds;
 
 		/**
 		 * @param IController $main
-		 * @return Mark
+		 * @return Mark|Mark[]
 		 * @throws APIException
 		 */
 		public function resolve(IController $main) {
+			if ($this->markId) {
+				return $this->getOne($main, $this->markId);
+			}
+
+			if ($this->markIds) {
+				return $this->getMultiple($main, $this->markIds);
+			}
+
+			throw new APIException(ErrorCode::NO_PARAM, null, "Arguments markId and markIds not specified");
+		}
+
+		/**
+		 * @param IController $main
+		 * @param int $markId
+		 * @return Mark
+		 */
+		private function getOne($main, $markId) {
 			$stmt = $main->makeRequest("SELECT * FROM `mark` WHERE `markId` = ?");
-			$stmt->execute([$this->markId]);
+			$stmt->execute([$markId]);
 			$item = $stmt->fetch(PDO::FETCH_ASSOC);
 
 			if (!$item) {
@@ -33,5 +53,18 @@
 			}
 
 			return new Mark($item);
+		}
+
+		/**
+		 * @param IController $main
+		 * @param int[] $markIds
+		 * @return Mark[]
+		 */
+		private function getMultiple($main, $markIds) {
+			$stmt = $main->makeRequest("SELECT * FROM `mark` WHERE `markId` IN (" . join(",", $markIds) . ")");
+			$stmt->execute();
+			$items = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+			return parseItems($items, "\\Model\\Mark");
 		}
 	}
