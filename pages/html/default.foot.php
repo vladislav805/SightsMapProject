17660a54955535b138b5029ad6840995d7655121
Vladislav Veluga <vladislav805@yandex.com>
2019-03-22 19:03:07 +0300

    Set year in footer

--- pages/html/default.foot.php
+++ pages/html/default.foot.php
@@ -17,7 +17,7 @@
 	</div>
 	<div class="footer-right">
 		<ul>
-			<li><a href="//velu.ga/" target="_blank" data-noAjax>velu.ga</a> &copy; 2015&ndash;2018</li>
+			<li><a href="//velu.ga/" target="_blank" data-noAjax>velu.ga</a> &copy; 2015&ndash;<?=date("Y");?></li>
 		</ul>
 	</div>
 </footer>
