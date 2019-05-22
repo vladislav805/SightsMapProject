bc4aeb021c333c7cfbd88150d455f7d271d40036
Vladislav Veluga <vladislav805@yandex.com>
2019-05-23 02:09:57 +0300

    Some editions in menu, fix notifications (remove from header, move to item in left menu)

--- pages/html/default.head.php
+++ pages/html/default.head.php
@@ -19,7 +19,7 @@
 <? if ($this->mController->getSession()) { ?>
 		<a class="material-icons head-element" href="/sight/add">add_location</a>
 <? } ?>
-		<div class="head-user head-element" data-feed-count="<?=$notificationCount ?? 0;?>">
+		<div class="head-user head-element">
 <?
 	if ($this->mController->getSession()) {
 		$u = $this->mController->getUser();
@@ -51,15 +51,16 @@
 				<li><a href="/map">Карта</a></li>
 				<li><a href="/sight/search">Поиск</a></li>
 				<li><a href="/sight/random">Случайное место</a></li>
-				<li><a href="/feed" id="head-events">События</a></li>
-				<li><a href="/login?action=logout&amp;repath=<?=htmlSpecialChars(get_http_request_uri());?>" data-noAjax>Выход</a></li>
+				<li><a href="/feed" id="head-events" data-feed-count="<?=$notificationCount ?? 0;?>">События</a></li>
+				<li><a href="/neural">Нейронка</a></li>
+				<li class="menu-item--userarea"><a href="/login?action=logout&amp;repath=<?=htmlSpecialChars(get_http_request_uri());?>" data-noAjax>Выход</a></li>
 <?
 	} else {
 ?>
 				<li><a href="/">Главная</a></li>
 				<li><a href="/sight/search">Поиск</a></li>
 				<li><a href="/sight/random">Случайное место</a></li>
-				<li><a href="#" data-noAjax onclick="return openLoginForm();">Вход / Регистрация</a></li>
+				<li><a class="menu-item--userarea" href="#" data-noAjax onclick="return openLoginForm();">Вход / Регистрация</a></li>
 <?
 	}
 ?>
