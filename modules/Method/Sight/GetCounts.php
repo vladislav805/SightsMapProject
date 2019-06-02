831195988dee876d38f31126388c2f2742a9a91b
Vladislav Veluga <vladislav805@yandex.com>
2019-06-02 22:41:35 +0300

    Add caching to redis result of sights.getCounts (used in index)

--- modules/Method/Sight/GetCounts.php
+++ modules/Method/Sight/GetCounts.php
@@ -12,22 +12,47 @@
 	 */
 	class GetCounts extends APIPublicMethod {
 
+		const KEY_TOTAL = "api_s_gc_total";
+		const KEY_VERIFIED = "api_s_gc_verified";
+		const KEY_ARCHIVED = "api_s_gc_archived";
+
 		/**
 		 * @param IController $main
 		 * @return array
 		 */
 		public function resolve(IController $main) {
-			$count = <<<SQL
+
+			// without redis 0.00088810920715332
+			// with redis    0.00021505355834961
+
+			$redis = $main->getRedis();
+
+			if ($redis->get(self::KEY_TOTAL)) {
+				$result = [
+					"total" => $redis->get(self::KEY_TOTAL),
+					"verified" => $redis->get(self::KEY_VERIFIED),
+					"archived" => $redis->get(self::KEY_ARCHIVED)
+				];
+			} else {
+
+
+				$count = <<<SQL
 SELECT 
        (SELECT COUNT(*) FROM `sight`) AS `total`,
        (SELECT COUNT(*) FROM `sight` WHERE `sight`.`isVerified` = 1) AS `verified`,
        (SELECT COUNT(*) FROM `sight` WHERE `sight`.`isArchived` = 1) AS `archived`
 SQL;
 
-			$stmt = $main->makeRequest($count);
+				$stmt = $main->makeRequest($count);
 
-			$stmt->execute();
-			$result = $stmt->fetch(PDO::FETCH_ASSOC);
+				$stmt->execute();
+				$result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+				$exOptions = ["EX" => 600];
+				$redis->set(self::KEY_TOTAL, $result["total"], $exOptions);
+				$redis->set(self::KEY_VERIFIED, $result["verified"], $exOptions);
+				$redis->set(self::KEY_ARCHIVED, $result["archived"], $exOptions);
+			}
 
 			return [
 				"total" => (int) $result["total"],
