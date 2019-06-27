b0f08e15b3d158e2d6d64f7f52e759e70c7d62bc
Vladislav Veluga <vladislav805@yandex.com>
2019-06-27 22:34:59 +0300

    Remove deprecated authorize (oauth)

--- modules/Method/Authorize/Authorize.php
+++ modules/Method/Authorize/Authorize.php
@@ -6,7 +6,6 @@
 	use Method\APIPublicMethod;
 	use Method\ErrorCode;
 	use Model\IController;
-	use Model\Session;
 	use ObjectController\UserController;
 
 	/**
@@ -14,9 +13,6 @@
 	 * @package Method\Authorize
 	 */
 	class Authorize extends APIPublicMethod {
-
-		protected $repath = null;
-		protected $access = 0;
 
 		/** @var string */
 		protected $login;
@@ -31,40 +27,19 @@
 		 */
 		public function resolve(IController $main) {
 
-			if (!$this->repath && (!$this->login || !$this->password) || $this->repath && !$this->access) {
-				throw new APIException(ErrorCode::NO_PARAM, null, "Login or password is empty");
+			if (!$this->login || !$this->password) {
+				throw new APIException(ErrorCode::NO_PARAM, null, "Login or password not specified");
 			}
 
-			$session = !$this->repath
-				? $this->login($main, $this->login, $this->password)
-				: $this->grant($main, $this->repath, $this->access);
+			$session = $main->perform(new DirectAuthorize(["login" => $this->login, "password" => $this->password]));
 
 			$json = $session->jsonSerialize();
 			if ($main instanceof \MainController) {
 				$main->setAuthKey($session->getAuthKey());
 			}
+
 			$json["user"] = (new UserController($main))->getById($session->getUserId(), ["photo", "city", "extended"]);
 			return $json;
 		}
 
-		/**
-		 * @param IController $main
-		 * @param string $login
-		 * @param string $password
-		 * @return Session
-		 */
-		private function login(IController $main, $login, $password) {
-			return $main->perform(new DirectAuthorize(["login" => $login, "password" => $password]));
-		}
-
-		/**
-		 * @param IController $main
-		 * @param string $repath
-		 * @param string $access
-		 * @return Session
-		 * @deprecated
-		 */
-		private function grant(IController $main, $repath, $access) {
-			return $main->perform(new GrantAuthorize(["repath" => $repath, "access" => $access]));
-		}
 	}
